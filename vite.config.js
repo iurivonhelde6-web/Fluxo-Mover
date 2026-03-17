50d455e439d842880b614164f7f5459b46e27707
IuriDev <iurivonhelde6@gmail.com>
2026-03-16 22:55:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,21 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true
-  }
+  },
+  build: {
+    // Aumenta o limite para 1000kb para silenciar o aviso
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        // Organiza as bibliotecas (Supabase, React, etc) em um arquivo separado
+        // Isso ajuda na performance e resolve o aviso de "chunk size"
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
 
